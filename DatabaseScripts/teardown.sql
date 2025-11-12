991b7e75da4b1e562660d51b408396ffe473d4bc
LumiUCN <10632106@ucn.dk>
2025-11-12 14:48:46 +0100

    Create teardown.sql
    Created teardown script. Simply undoes setup in reverse order, aside from removing database.

---
+++ DatabaseScripts/teardown.sql
@@ -0,0 +1,18 @@
+Use [InfiniteInfluence]
+ALTER TABLE [AnnouncementInfluencer] DROP Constraint [CHK_FullAnnouncement]
+GO
+
+DROP FUNCTION [dbo].[AvailableSpots]
+GO
+
+DROP TABLE [AnnouncementInfluencer]
+GO
+
+DROP TABLE [Announcement]
+GO
+
+DROP TABLE [Company]
+GO
+
+DROP TABLE [Influencer]
+GO
