4ea5a1adb5dcfbdc4dfda3d0b5d6bb8f80d29e7b
LumiUCN <10632106@ucn.dk>
2025-11-13 12:56:12 +0100

    Fixed Name Typo
    AnnouncementController was called AnnouncementController1

---
+++ WebClient/Controllers/AnnouncementController.cs
@@ -0,0 +1,93 @@
+﻿using DataAccessLibrary.Daoclasser;
+using DataAccessLibrary.Interfaces;
+using DataAccessLibrary.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class AnnouncementController : Controller
+    {
+        private IAnnouncementDao _announcementDao;
+
+        public AnnouncementController()
+        {
+            _announcementDao = DataAccessLibrary.DefaultValues.DefaultAnnouncementDao;
+        }
+
+        // GET: AnnouncementController1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AnnouncementController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AnnouncementController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AnnouncementController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Announcement announcement)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AnnouncementController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AnnouncementController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Announcement announcement)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AnnouncementController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AnnouncementController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Announcement announcement)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
