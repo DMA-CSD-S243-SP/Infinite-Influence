3b17b408b6f5c0d31d5223fd9a126b5b73ad72a9
LumiUCN <10632106@ucn.dk>
2025-11-13 12:43:05 +0100

    Added static default values

---
+++ DataAccessLibrary/DefaultValues.cs
@@ -0,0 +1,7 @@
+﻿namespace DataAccessLibrary
+{
+    public static class DefaultValues
+    {
+        public static string DefaultConnectionString { get { return "\"Data Source=localhost;Persist Security Info=True;User ID=sa;Password=@12tf56so;Encrypt=True;Trust Server Certificate=True\"";  } }
+    }
+}
