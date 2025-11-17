35828e22549a30358584070029c4b5bec4ae00c4
Anders-Have <10632098@ucn.dk>
2025-11-17 10:35:28 +0100

    updated AnnoucementController
    added GetAll and getOne methods.

--- DatabaseAPI/Controllers/AnnouncementController.cs
+++ DatabaseAPI/Controllers/AnnouncementController.cs
@@ -32,4 +32,35 @@ public class AnnouncementController : ControllerBase
             return StatusCode(500, "An error occurred while trying to create an Announcement");
         }
     }
+
+    [HttpGet("{id}")]
+    public ActionResult<Announcement> Get(int id)
+    {
+        try
+        {
+            var announcement = _announcementDao.GetAnnouncement(id);
+            if (announcement == null)
+            {
+                return NoContent();
+            }
+            return Ok(announcement);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while trying to get the Announcement");
+        }
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Announcement>> GetAll()
+    {
+        try
+        {
+            return Ok(_announcementDao.GetAllAnnouncements());
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while trying to get all Announcements");
+        }
+    }
 }
